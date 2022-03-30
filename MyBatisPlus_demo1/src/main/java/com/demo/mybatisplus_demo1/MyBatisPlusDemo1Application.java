ecae15bf9e6a9594255037409a7100f04dd48a0f
wanyu747839@outlook.com <wanyu747839@outlook.com>
2022-03-30 10:29:23 +0800

    mybatis plus

---
+++ MyBatisPlus_demo1/src/main/java/com/demo/mybatisplus_demo1/MyBatisPlusDemo1Application.java
@@ -0,0 +1,13 @@
+package com.demo.mybatisplus_demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisPlusDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisPlusDemo1Application.class, args);
+    }
+
+}
---
+++ MyBatisPlus_demo1/src/test/java/com/demo/mybatisplus_demo1/MyBatisPlusDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.mybatisplus_demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisPlusDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
